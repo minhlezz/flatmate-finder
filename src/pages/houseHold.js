ed22b7b15592eed1bc2380efb77836c5b9f8c68e
minhlezz <kuco007@gmail.com>
2021-02-02 12:06:21 +0700

    refactor layout household flatmate add footer

--- src/pages/houseHold.js
+++ src/pages/houseHold.js
@@ -9,8 +9,8 @@ import HouseFilterModal from '../components/HouseHoldComponent/HouseHoldModal/ho
 import { HOME_FILTER } from '../utils/graphql';
 
 function HouseHold() {
-    const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0cMhRaPgqVMdYwCsU8eje4v6gOkj962GL3g&usqp=CAU"
-    const avt = "https://noidangsong.vn/files/uploads/fb1735058496563345/1526444239-tt_avatar_small.jpg"
+    const url = "https://abcland.vn/wp-content/uploads/2020/07/Apartment-la-gi.jpg"
+    const avt = "https://www.udiscovermusic.com/wp-content/uploads/2019/11/The-Weekend-Heartless.jpg"
     const [show, setShow] = useState(false);
     const [filterModal, setFilterModal] = useState(false);
     const handleClose = () => setShow(false);
@@ -24,7 +24,7 @@ function HouseHold() {
     useEffect(() => {
         //render data at initialize
         executeFilter();
-    });
+    }, []);
 
     if (filterLoading) return <Spinner animation="border" />
 
@@ -39,87 +39,84 @@ function HouseHold() {
             return (
                 <Fragment key={index}>
                     <Card className='card-main'>
-                        <Col className="card-content">
-                            <Card.Img src={url}
-                                style={
-                                    {
-                                        width: '200px',
-                                        height: '200px'
-                                    }}
-                            />
-                            <Card.Body>
-                                <Card.Subtitle>
-                                    {house.houseTitle ? house.houseTitle : 'aA'}
-                                </Card.Subtitle>
-                                <small className="text-muted">address- number Number street Street, city City,</small>
-                                <p>
-                                    {house.bed ? house.bed : 'aA'}
-                                    <Icon disabled
-                                        size='large'
-                                        name='bed' />
-                                    {house.bath ? house.bath : 'aA'}
-                                    <Icon disabled
-                                        size='large'
-                                        name='bath' />
-                                    <Icon disabled
-                                        size='large'
-                                        name='square full' />
-                                    {house.area ? house.area : 'aA'}m2
-                                </p>
 
-                                <p>
-                                    <Icon disabled
-                                        size='large'
-                                        name='dollar' />
-                                    {house.budget ? house.budget : 'aA'}
-                                </p>
-                                <p>
-                                    {house.houseDescription ? house.houseDescription : 'aA'}
-                                </p>
-                                <NavLink to={`/household/${houseHoldID}`} >
-                                    <Icon.Group size='large'>
-                                        <Icon loading size='big' name='circle notch'
-                                            inverted
-                                            color='black' />
+                        <Card.Img src={url} />
 
+                        <Card.Body>
+                            <Row>
+                                <Col>
+
+                                    <Card.Title><strong>{house.houseTitle ? house.houseTitle : 'aA'}</strong> </Card.Title>
+
+                                    {/* <small className="text-muted">address- number Number street Street, city City,</small> */}
+                                    <Card.Text>
+                                        <Icon
+                                            size='large'
+                                            name='dollar' />
+                                        {house.budget ? house.budget : 'aA'}
+                                    </ Card.Text>
+
+                                    <Card.Text>
+                                        <Icon
+                                            color='color-dg'
+                                            size='large'
+                                            name='bed' />
+                                        {house.bed ? house.bed : 'aA'}
+
+                                        <Icon
+                                            color='color-dg'
+                                            size='large'
+                                            name='bath' />
+                                        {house.bath ? house.bath : 'aA'}
+
+                                        <Icon
+                                            color='color-dg'
+                                            size='large'
+                                            name='square full' />
+                                        {house.area ? house.area : 'aA'}m2
+                                    </ Card.Text>
+
+                                    <Button className="btn-fmCard" as={NavLink} to={`/household/${houseHoldID}`} >View</Button>
+                                    {/* <NavLink to={`/household/${houseHoldID}`} >
                                         <Icon name='home'
                                             inverted
                                             color='black'
+                                            size="big"
                                         />
-                                    </Icon.Group>
-                                </NavLink>
-                            </Card.Body>
-                            <Col className="card-owner">
-                                <Image
-                                    src={avt}
-                                    style={
-                                        {
-                                            width: '82px',
-                                            height: '82px',
-                                            margin: '5px'
-                                        }}
-                                    roundedCircle />
-                                <NavLink to={`/flatmate/${flatmateID}`} >
+                                    </NavLink> */}
+                                </Col>
+
+                                {/* House owner */}
+                                <Col xs={3} className="card-owner">
+                                    <Image
+                                        src={avt}
+                                        style={
+                                            {
+                                                width: '82px',
+                                                height: '82px',
+                                                margin: '5px'
+                                            }}
+                                        roundedCircle />
+                                    <NavLink to={`/flatmate/${flatmateID}`} >
+                                        <Icon
+                                            inverted
+                                            color='color-dg'
+                                            size='big'
+                                            name='address card outline' />
+                                    </NavLink>
                                     <Icon
                                         inverted
-                                        color='black'
+                                        color='color-dg'
                                         size='big'
-                                        name='address card outline' />
-                                </NavLink>
-
-                                <Icon
-                                    inverted
-                                    color='black'
-                                    size='big'
-                                    name='chat' />
-                                <Icon
-                                    inverted
-                                    color='black'
-                                    size='big'
-                                    name='phone' />
-                            </Col>
-
-                        </Col>
+                                        name='chat' />
+                                    <Icon
+                                        inverted
+                                        color='color-dg'
+                                        size='big'
+                                        name='phone' />
+                                </Col>
+                            </Row>
+                        </Card.Body>
                     </Card>
                 </Fragment>
             )
@@ -128,39 +125,42 @@ function HouseHold() {
 
 
     return (
-        <Row>
-            <Col xs={12} md={8}>
-                <Col className="household-addingg">
-                    <h3>Add your home there...  </h3>
-                    <Button
-                        style={{
-                            marginLeft: '5px'
-                        }}
-                        onClick={handleShow}
-                    >
-                        <Icon name='add' />
-                    </Button>
-                    <Button
-                        style={{
-                            marginLeft: '5px'
-                        }}
-                        onClick={handleShowFilterModal}
-                        variant="secondary"
-                    >
-                        <Icon name='filter' />
-                    </Button>
-                </Col>
-                <HouseHoldModal show={show} handleClose={handleClose} />
-                {/* Filtering  */}
-                <HouseFilterModal
-                    filterModal={filterModal}
-                    handleCloseFilterModal={handleCloseFilterModal}
-                    executeFilter={executeFilter}
-                />
-                {/* ////////////////// */}
-                <h3>Finding "..." results</h3>
+        <Row xs={12} md={8}>
+            <Row className="household-addingg">
+                <h3>Add your home there...  </h3>
+                <Button
+                    style={{
+                        marginLeft: '5px'
+                    }}
+                    onClick={handleShow}
+                >
+                    <Icon name='add' />
+                </Button>
+                <Button
+                    style={{
+                        marginLeft: '5px'
+                    }}
+                    onClick={handleShowFilterModal}
+                    variant="secondary"
+                >
+                    <Icon name='filter' />
+                </Button>
+            </Row>
+            {/* Add new HouseHold */}
+
+            <HouseHoldModal show={show} handleClose={handleClose} />
+
+            {/* Filtering  */}
+            <HouseFilterModal
+                filterModal={filterModal}
+                handleCloseFilterModal={handleCloseFilterModal}
+                executeFilter={executeFilter}
+            />
+
+            {/* Household*/}
+            <Row className="card-display">
                 {houseHoldMarkUp}
-            </Col>
+            </Row>
         </Row >
     )
 }
